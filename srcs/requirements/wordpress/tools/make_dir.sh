ad61acb931df806bd9c71aaac9a9ae6324febc82
Tosh-Sharma <toshkumar.s@somaiya.edu>
2024-03-06 13:38:09 +0100

    Updated nginx config and wordpress dockerfile

--- srcs/requirements/wordpress/tools/make_dir.sh
+++ srcs/requirements/wordpress/tools/make_dir.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
 if [ ! -d "/Users/toshsharma/" ]; then
 	mkdir /Users/toshsharma/
-	mkdir /Users/toshsharma/wordpress
-	mkdir /Users/toshsharma/mariadb
+fi
+
+if [ ! -d "/Users/toshsharma/data" ]; then
+	mkdir /Users/toshsharma/data
+fi
+
+if [ ! -d "/Users/toshsharma/data/wordpress" ]; then
+	mkdir /Users/toshsharma/data/wordpress
+fi
+
+if [ ! -d "/Users/toshsharma/data/mariadb" ]; then
+	mkdir /Users/toshsharma/data/mariadb
 fi
