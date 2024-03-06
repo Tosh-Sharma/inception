d049079f9e2ef4866aa989a30866ae83a6f32244
Tosh-Sharma <toshkumar.s@somaiya.edu>
2024-03-06 14:41:58 +0100

    Minor code changes

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME};" >> /test.txt
+echo "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';" >> /test.txt
+echo "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';" >> /test.txt
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';" >> /test.txt
+
 service mariadb start;
 
 mariadb -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
